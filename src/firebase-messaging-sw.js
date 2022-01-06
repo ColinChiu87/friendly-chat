b32241cfc61cbabef55b082fdca326565b674165
ColinChiu87 <colinchiu87@gmail.com>
2022-01-06 15:21:45 +0800

    finish this project

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,1 +1,10 @@
 // TODO: Set up Firebase Cloud Messaging service worker
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging/sw';
+import { getFirebaseConfig } from './firebase-config';
+
+const firebaseApp = initializeApp(getFirebaseConfig());
+getMessaging(firebaseApp);
+console.info('Firebase messaging service worker is set up');
+
+//Service Worker 只需要加載和初始化 Firebase Cloud Messaging SDK，它將負責顯示通知。
